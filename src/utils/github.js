201ac87ed27c56bc32922338c99adeb2f6ad7774
linyunjun <49714339+linyunjun@users.noreply.github.com>
2024-10-24 01:16:23 +0800

    Update github.js

--- src/utils/github.js
+++ src/utils/github.js
@@ -72,6 +72,7 @@ async function fetchAllEvents() {
             });
 
             const message = 'Message for test';
+            const canBeRemove = 'canBeRemove';
 
             // Check for API rate limit or pagination issues
             if (events.length === 0) {
